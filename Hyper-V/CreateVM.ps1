629cd706b47b8b057a4c4a7135a0f62498e944d5
bk147 <bkirk@live.dk>
2015-09-09 08:43:20 +0200

    Basic VM Creation and deletion
    Creates a VM with basic settings using a template.
    Destroys a vm with all attached harddisks.

---
+++ Hyper-V/CreateVM.ps1
@@ -0,0 +1,70 @@
+﻿param (
+    [parameter(Mandatory)][string[]] $Namelist,
+#    [string] $Template = 'ws2016tp3_sysprepped.vhdx',
+    [string] $Template = 'WS2K12R2SP1_20150702_sysprepped.vhdx',
+    [string] $Answerfile = 'Unattend_templ.xml',
+    [string] $Hypervroot = 'C:\_Hyper-V',
+    [string] $VMSwitch = 'Internal',
+    [switch] $Start = $false
+)
+
+
+$answerfilepath = $hypervroot + "\" + $answerfile
+if (!(Test-Path $answerfilepath)) { Write-Error "$answerfilepath does not exist" ; Exit }
+$templatepath = $hypervroot + '\Virtual Hard Disks\' + $template
+if (!(Test-Path $templatepath)) { Write-Error "$templatepath does not exist" ; Exit }
+
+function CreateUnattendXml()
+{
+    Param(
+        [string]$templatePath,
+        [string]$machineName,
+        [string]$destinationPath
+    )
+    
+    [xml] $config = [xml] (Get-Content $templatePath)
+
+    $node = $config.unattend.settings  | ? { $_.pass -eq "specialize" }
+    $snode = $node.component | ? {$_.name -eq "Microsoft-Windows-Shell-Setup"}
+    $snode.ComputerName = $machineName
+
+    if (Test-Path $destinationPath) { Remove-Item $destinationPath -Force }
+
+    $config.Save($destinationPath)
+}
+
+foreach ($vmname in $Namelist) {
+    "Creating a VM ($vmname) using '$Template'"
+
+    $vhdpath = $Hypervroot + '\Virtual Hard Disks\' + $vmname + '.vhdx'
+    if (Test-Path $vhdpath) {
+        Write-Warning "$vhdpath already exists - will not create..."
+    } else {
+        "  Creating VHD using '$Answerfile'..."
+        $vhd = New-VHD -Path $vhdpath -ParentPath $templatepath
+        $mp = Mount-VHD -Path $vhdpath -NoDriveLetter -Passthru
+        $tmppathname = [DateTime]::Now.ToFileTime()
+        $tmppath = New-Item -Path "$Hypervroot\Virtual Hard Disks\$tmppathname" -ItemType Directory
+        $setuppath = "$tmppath\Windows\Setup\Scripts"
+        $part = ($mp | Get-Disk | Get-Partition | Sort-Object Size)[-1] #Get biggest partition
+        $part | Add-PartitionAccessPath -AccessPath $tmppath.FullName
+        CreateUnattendXml -templatePath $answerfilepath -machineName $vmname -destinationPath "$tmppath\Unattend.xml"
+        if (!(Test-Path $setuppath)) { $null = New-Item $setuppath -ItemType Directory }
+        "del C:\Unattend.xml" | Out-File "$setuppath\SetupComplete.cmd" -Encoding ascii
+        $part | Remove-PartitionAccessPath -AccessPath $tmppath.FullName
+        Dismount-VHD -Path $vhdpath
+        Remove-Item -Path "$Hypervroot\Virtual Hard Disks\$tmppathname"
+    }
+
+    $vm = Get-VM $vmname -ErrorAction SilentlyContinue
+    if ($vm -ne $empty) {
+        Write-Warning "$vmname already exists - will not create..."
+    } else {
+        "  Create vm with the '$VMSwitch' switch..."
+        $vm = New-VM -Name $vmname -MemoryStartupBytes 512MB -Generation 2 -SwitchName $VMSwitch -VHDPath $vhdpath
+    }
+    #Enable "Guest Services" so that we can copy files to the vm without network...
+    $vm | Enable-VMIntegrationService -Name "Guest Service Interface"
+    $vm | Set-VM -MemoryMinimumBytes 64MB -MemoryMaximumBytes 2GB
+    if ($Start) { $vm | Start-VM }
+}
---
+++ Hyper-V/DestroyVM.ps1
@@ -0,0 +1,19 @@
+﻿param (
+    [parameter(Mandatory=$true)]
+    [string[]]$VMList
+)
+
+foreach ($vmname in $VMList) {
+    $vm = Get-VM $vmname -ErrorAction SilentlyContinue
+    "Destroying $vmname..."
+    if ($vm) {
+        $disklist = $vm | Get-VMHardDiskDrive
+        foreach ($disk in $disklist) {
+            "Removing '$($disk.Path)'..."
+            $null = Remove-Item $disk.Path
+        }
+        $vm | Remove-VM -Force
+    } else {
+        Write-Warning "$vmname not found - ignoring..."
+    }
+}
